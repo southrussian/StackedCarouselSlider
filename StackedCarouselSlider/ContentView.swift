a434433a5ec6f9aa13a2f89a60695499fcd9fb4c
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-12 23:29:00 +0300

    Initial Commit

---
+++ StackedCarouselSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StackedCarouselSlider
+//
+//  Created by Danil Peregorodiev on 12.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StackedCarouselSlider/StackedCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedCarouselSliderApp.swift
+//  StackedCarouselSlider
+//
+//  Created by Danil Peregorodiev on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct StackedCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
